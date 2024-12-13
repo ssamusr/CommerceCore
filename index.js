fda731625a2714ad89c2a51aa6dc18db4d08445b
Samuel <samusr@gmail.com>
2024-12-13 16:03:38 +0000

    Installed cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const express = require("express");
-const app = express();
-const port = 3000;
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const port = 3000
 
-app.use(express.static("./public"));
-app.use("/", require("./src/routes/index"));
+app.use(express.static('./public'))
+app.use(cors())
+app.use('/', require('./src/routes/index'))
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+  console.log(`Example app listening on port ${port}`)
+})
